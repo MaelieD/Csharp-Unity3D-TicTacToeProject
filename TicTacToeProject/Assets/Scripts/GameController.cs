249c94c2b8f06f52f5f08695106bf5cc3791c388
MaelieD <dessapt@et.esiea.fr>
2016-06-10 00:46:32 +0200

    -NEW : game over system

--- TicTacToeProject/Assets/Scripts/GameController.cs
+++ TicTacToeProject/Assets/Scripts/GameController.cs
@@ -5,10 +5,17 @@ using System.Collections;
 public class GameController : MonoBehaviour {
 
 	public Text[] buttonList;
+	private string playerSide;
+	public GameObject gameOverPanel;
+	public Text gameOverText;
+	private int moveCount;
 
 	void Awake()
 	{
 		SetGameControllerReferenceOnButtons ();
+		playerSide = "X";
+		gameOverPanel.SetActive (false);
+		moveCount = 0;
 	}
 
 	void SetGameControllerReferenceOnButtons ()
@@ -20,12 +27,74 @@ public class GameController : MonoBehaviour {
 
 	public string GetPlayerSide() 
 	{
-		return "?";
+		return playerSide;
+	}
+
+	void ChangeSides () {
+		playerSide = (playerSide == "X") ? "O" : "X";
 	}
 
 	public void EndTurn()
 	{
-		Debug.Log ("EndTurn is not implemented !");
+		moveCount++;
+
+		if (buttonList [0].text == playerSide && buttonList [1].text == playerSide && buttonList [2].text == playerSide)
+		{
+			GameOver();
+		}
+
+		if (buttonList [3].text == playerSide && buttonList [4].text == playerSide && buttonList [5].text == playerSide)
+		{
+			GameOver();
+		}
+
+		if (buttonList [6].text == playerSide && buttonList [7].text == playerSide && buttonList [8].text == playerSide)
+		{
+			GameOver();
+		}
+
+		if (buttonList [0].text == playerSide && buttonList [3].text == playerSide && buttonList [6].text == playerSide)
+		{
+			GameOver();
+		}
+
+		if (buttonList [1].text == playerSide && buttonList [4].text == playerSide && buttonList [7].text == playerSide)
+		{
+			GameOver();
+		}
+
+		if (buttonList [2].text == playerSide && buttonList [5].text == playerSide && buttonList [8].text == playerSide)
+		{
+			GameOver();
+		}
+
+		if (buttonList [0].text == playerSide && buttonList [4].text == playerSide && buttonList [8].text == playerSide)
+		{
+			GameOver();
+		}
+
+		if (buttonList [2].text == playerSide && buttonList [4].text == playerSide && buttonList [6].text == playerSide)
+		{
+			GameOver();
+		}
+
+		if (moveCount >= 9) {
+			SetGameOverText("It's a draw !");
+		}
+
+		ChangeSides ();
+	}
+
+	void GameOver() {
+		for (int i = 0; i < buttonList.Length; i++) {
+			buttonList [i].GetComponentInParent<Button> ().interactable = false;
+		}
+		SetGameOverText (playerSide + " Wins !");
+	}
+
+	void SetGameOverText(string value){
+		gameOverPanel.SetActive (true);
+		gameOverText.text = value;
 	}
 
 }
